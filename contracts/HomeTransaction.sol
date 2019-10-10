56be5494a639cf3e1945138aaa89b44167ea3dcd
Alexander Schrab <alexander.schrab@gmail.com>
2019-10-10 15:13:25 +0200

    More contract code

--- contracts/HomeTransaction.sol
+++ contracts/HomeTransaction.sol
@@ -10,20 +10,31 @@ contract HomeTransaction {
     // Set at deposit
     address public buyer;
     uint public deposit;
+    bool public finalized = false;
     
-    function setSeller(address payable _seller, uint _cost) public {
+    function realtorSetupContract(address payable _seller, uint _cost) public {
         require(msg.sender == realtor, "Only realtor can set seller");
-        require(seller == address(0) || buyer == address(0), "Cannot reset seller when there is a buyer");
+        require(seller == address(0), "Seller already set");
+        
         seller = _seller;
         cost = _cost;
     }
     
-    function payDeposit() payable public {
+    function buyerSignContract() payable public {
         require(seller != address(0), "Realtor needs to set seller and cost first");
-        require(buyer == address(0), "Contract already has a buyet");
-        require(msg.value >= cost/10 && msg.value <= cost, "You have to deposit between 10% and 100%");
+        require(buyer == address(0), "Contract already has a buyer");
+        require(msg.value >= cost/10 && msg.value <= cost, "Buyer needs to deposit between 10% and 100% to sign contract");
         
         buyer = msg.sender;
         deposit = msg.value;
+    }
+    
+    function buyerFinalizeTransaction() payable public {
+        require(buyer == msg.sender, "Only buyer can finalize transaction");
+        require(msg.value + deposit == cost, "Buyer needs to pay the rest of the cost to finalize transaction");
+        require(!finalized, "Cannot finalize already finalized transaction");
+        
+        finalized = true;
+        seller.transfer(cost);
     }
 }
