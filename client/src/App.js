05c2e130afa85da8a0402d50f3b865f53f8412b5
Tony Jin <nijynot@gmail.com>
2019-10-11 11:21:19 +0200

    Remove git conflict

--- client/src/App.js
+++ client/src/App.js
@@ -27,14 +27,6 @@ const App = () => {
         console.error("FAIL");
       }
       const accounts = await web3.eth.getAccounts();
-<<<<<<< HEAD
-=======
-      const contract = new web3.eth.Contract(
-        HomeTransaction.abi,
-        "0x5af4dae68b0b28912da9955b3aa67246946bd9e7"
-      );
->>>>>>> e6ae3bbf8eb9e76650786cdbe1c3c89fa07d9635
-
       const factory = new web3.eth.Contract(
         Factory.abi,
         '0x2F312Dd912407C11AAb7488e261afd8fAEeE23EF'
@@ -42,7 +34,7 @@ const App = () => {
       factory.methods.getInstances().call().then((res) => {
         setContracts(res);
       });
-      
+
       setState({ accounts, contract });
     };
 
