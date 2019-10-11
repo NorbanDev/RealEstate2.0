182071a551c294859834d2bb05f198419753ab77
Tony Jin <nijynot@gmail.com>
2019-10-11 13:46:57 +0200

    Add classnames and styling for contractt page

--- client/src/pages/Contract.js
+++ client/src/pages/Contract.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
 import { Link, useParams } from "react-router-dom";
+import cx from 'classnames';
 import './Contract.scss';
+
+const timeline = [
+  { text: 'Contract created' },
+  { text: 'Buyer approve contract' },
+  { text: 'Bank sign and deposit' },
+  { text: '(Optional) Buy opt-out' },
+  { text: '(Optional) After 5 min, everyone can opt-out' },
+  { text: 'Buyer finalize transaction' },
+];
 
 export default function Contract(props) {
   let { contractAddr } = useParams();
   const [progress, setProgress] = useState(50);
+  const [timelineProgress, setTimelineProgress] = useState(1);
 
   return (
     <div className="ContractPage">
       <div className="ContractPage-body">
         <h1>Contract</h1>
         <span className="ContractPage-addr">{contractAddr}</span>
+        <div className="Timeline">
+          {timeline.map((point, i) => (
+            <div
+              className={cx('Timeline-point', {
+                done: timelineProgress > i,
+                'in-progress': timelineProgress == i,
+              })}
+            >{i + 1}. {point.text}</div>
+          ))}
+        </div>
         <div className="ProgressBar-container">
           <div className="ProgressBar-background"></div>
           <div
