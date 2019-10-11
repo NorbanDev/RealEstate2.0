76563a3ae6b0bf396f6bf1de2b00419c8a23ca8c
Alexander Schrab <alexander.schrab@gmail.com>
2019-10-11 15:15:23 +0200

    Add simple error handling

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -6,28 +6,39 @@ import Seller from "./pages/Seller";
 import Buyer from "./pages/Buyer";
 import Coop from "./pages/Coop";
 import Contract from "./pages/Contract";
+import Loading from "./pages/Loading";
 
-const AppRouter = ({ account, homeTransactions }) => (
-  <Router>
-    <Switch>
-      <Route
-        exact
-        path="/"
-        render={() => <Main homeTransactions={homeTransactions} />}
-      />
-      <Route
-        path="/:index"
-        render={({
-          match: {
-            params: { index }
-          }
-        }) => <Contract homeTransaction={homeTransactions && homeTransactions[index]} />}
-      />
-      <Route path="/seller" render={() => <Seller account={account} />} />
-      <Route path="/buyer" component={Buyer} />
-      <Route path="/coop" component={Coop} />
-    </Switch>
-  </Router>
-);
+const AppRouter = ({ account, homeTransactions, web3error }) => {
+  if (!account || !homeTransactions) {
+    return <Loading web3error={web3error} />;
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <Main homeTransactions={homeTransactions} />}
+        />
+        <Route
+          path="/:index"
+          render={({
+            match: {
+              params: { index }
+            }
+          }) => (
+            <Contract
+              homeTransaction={homeTransactions && homeTransactions[index]}
+            />
+          )}
+        />
+        <Route path="/seller" render={() => <Seller account={account} />} />
+        <Route path="/buyer" component={Buyer} />
+        <Route path="/coop" component={Coop} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default AppRouter;
