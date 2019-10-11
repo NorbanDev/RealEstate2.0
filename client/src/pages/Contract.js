537ccdecedbc3970d11d0f4a16dfe7a596ca82a7
Tony Jin <nijynot@gmail.com>
2019-10-11 15:22:03 +0200

    contract things

--- client/src/pages/Contract.js
+++ client/src/pages/Contract.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Link, useParams } from 'react-router-dom';
 import cx from "classnames";
 import "./Contract.scss";
 
@@ -12,15 +13,44 @@ const timeline = [
 ];
 
 export default function Contract({ homeTransaction }) {
-  const [progress /*, setProgress*/] = useState(50);
-  // const homeTransaction = homeTransactions[index];
-  const [timelineProgress /*, setTimelineProgress*/] = useState(1);
+  const { index } = useParams();
+  const [progress, setProgress] = useState(10);
+  const [timelineProgress, setTimelineProgress] = useState(1);
+
+  const updateProgress = (index) => {
+    const percent = (index) / timeline.length;
+
+    setProgress(Math.min(percent * 100, 100));
+    setTimelineProgress(index);
+  }
+
+  useEffect(() => {
+    (async () => {
+      if (homeTransaction) {
+        const sellerSigned = await homeTransaction.methods.sellerSigned().call();
+        const buyerSigned = await homeTransaction.methods.buyerSigned().call();
+        const finalized = await homeTransaction.methods.finalized().call();
+
+        console.log(sellerSigned, buyerSigned, finalized);
+      }
+    })();
+  }, [homeTransaction]);
 
   return (
     <div className="ContractPage">
       <div className="ContractPage-body">
         <h1>Contract</h1>
         <span className="ContractPage-addr">{homeTransaction && homeTransaction.options.address}</span>
+
+        <Route exact path="/:addr" render={() => (
+          <div className="Contract-tabs">
+            <Link to={`/${index}/buyer`}>Buyer</Link>
+            <Link to={`/${index}/seller`}>Seller</Link>
+          </div>
+        )} />
+        <Route path="/:addr/buyer" render={() => (
+          <div>buyer!!!!</div>
+        )} />
         <div className="Timeline">
           {timeline.map((point, i) => (
             <div
